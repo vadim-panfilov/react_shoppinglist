f8d25b6b73212e47ca4da5699509bca01706bc8a
Vadim Panfilov <vadimpanfilov@Vadim-MacBook-Pro.local>
2021-10-28 20:35:32 +0300

    react created and added to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const URL = 'http://localhost:8888/ostokset';
+
+const getProducts = () => {
+  return axios.get(URL + '/index.php', {
+    headers:{
+      'Content-Type': 'application/json'
+  }
+  })
+};
+
+const createProduct = (product) => {
+  return axios.post(URL + "/save.php", product);
+};
+
+const deleteProduct = (id) => {
+   return axios.get(URL + "/delete.php" + '?id=' + id);
+};
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+
+  useEffect(() => {
+     getProducts().then(response => {
+      setProducts(response.data)
+    })
+  }, [])
+
+  const hanldleAdd = async () => {
+    try {
+      const response = await createProduct({description, amount})
+      setDescription('')
+      setAmount('')
+      setProducts([response.data, ...products])
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  }
+
+  const hanldleDelete = async (event, id) => {
+
+    event.preventDefault()
+    try {
+      await deleteProduct(id);
+      setProducts(products.filter((product) => product.id !== id))
+      
+    } catch (error) {
+      console.log('error: ', error);
+      
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Shopping list</h3>
+      <div style={{ marginBottom: 30, display: "flex", gap: 10 }}>
+        <span>New item</span>
+        <input
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+          value={description}
+          placeholder="type description"
+        />
+        <input
+          onChange={(event) => {
+            setAmount(event.target.value);
+          }}
+          value={amount}
+          type="number"
+          placeholder="type amount"
+        />
+        <button onClick={hanldleAdd}>Add</button>
+      </div>
+      <table>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td>{product.description}</td>
+            <td>{product.amount}</td>
+            <td>
+              <a onClick={(event) => hanldleDelete(event, product.id)} href="#">
+                Delete
+              </a>
+            </td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
